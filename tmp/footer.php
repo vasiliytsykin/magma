6693fdd5d6c40ac06c1665b7bd480a24cba99f65
vasiliytsykin <vasiliytsykin@gmail.com>
2016-07-22 09:51:21 +0500

    copyright added

--- tmp/footer.php
+++ tmp/footer.php
@@ -5,57 +5,58 @@
 
         <div class="container-fluid">
             <div class="footer-over">
-
-<!--                <div class="colarea">-->
-
-                    <div class="col-lg-3 col-md-3 col-sm-3 no-mobile">
-                        <h4><a href="#">Продукция</a></h4>
-                        <menu>
-                            <li><a href="#">Гибриды</a></li>
-                            <li><a href="#">Видеорегистраторы</a></li>
-                            <li><a href="#">Радар-детктор</a></li>
-                            <li><a href="#">Системы парковки</a></li>
-                            <li><a href="#">Инверторы</a></li>
-                            <li><a href="#">Аксессуары</a></li>
-                        </menu>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-3 no-mobile">
-                        <h4><a href="#">Дилерам</a></h4>
-                        <menu>
-                            <li><a href="#">Точки продаж</a></li>
-                            <li><a href="#">Список дилеров</a></li>
-                        </menu>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-3 no-mobile">
-                        <h4><a href="#">О нас</a></h4>
-                        <menu>
-                            <li><a href="#">Контакты</a></li>
-                            <li><a href="#">Помощь в покупке</a></li>
-                            <li><a href="#">FAQ</a></li>
-                        </menu>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                        <h4><a href="#">Обратная связь</a></h4>
-                        <div class="f-contacts">
-                            <div><a href="#">+7 984 56 76 556</a></div>
-                            <div><a href="#">magma@mail.ru</a></div>
+                <div class="col-lg-3 col-md-3 col-sm-3 no-mobile">
+                    <h4><a href="#">Продукция</a></h4>
+                    <menu>
+                        <li><a href="#">Гибриды</a></li>
+                        <li><a href="#">Видеорегистраторы</a></li>
+                        <li><a href="#">Радар-детктор</a></li>
+                        <li><a href="#">Системы парковки</a></li>
+                        <li><a href="#">Инверторы</a></li>
+                        <li><a href="#">Аксессуары</a></li>
+                    </menu>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 no-mobile">
+                    <h4><a href="#">Дилерам</a></h4>
+                    <menu>
+                        <li><a href="#">Точки продаж</a></li>
+                        <li><a href="#">Список дилеров</a></li>
+                    </menu>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 no-mobile">
+                    <h4><a href="#">О нас</a></h4>
+                    <menu>
+                        <li><a href="#">Контакты</a></li>
+                        <li><a href="#">Помощь в покупке</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </menu>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                    <h4><a href="#">Обратная связь</a></h4>
+                    <div class="f-contacts">
+                        <div><a class="footer-phone" href="#">+7 984 56 76 556</a></div>
+                        <div><a class="footer-email" href="#">magma@mail.ru</a></div>
+                        <div class="social-icons">
                             <a href="#" class="facebook-icon"></a>
                             <a href="#" class="twitter-icon"></a>
                         </div>
                     </div>
-
-<!--                </div>-->
-
+                </div>
             </div>
         </div>
 
-
-
     </footer>
 
+    <div class="copyright">
+        <div class="tc">
+            <div class="td">
+                <p>
+                    © 2010-2016 Magma. Автоэлектроника и аксесуары.</br>
+                    Все права защищены. Информация на сайте не является публичной офертой
+                </p>
+            </div>
+        </div>
+    </div>
 
 <?php foreach($arScript as $arItem){?><script src="<?=$arItem;?>"></script><?}?>
 
