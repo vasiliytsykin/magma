d646552801f32599322d0e2c32cbc0ceafe537fe
Vasiliy Tsykin <vasiliytsykin@gmail.com>
2016-07-22 18:52:12 +0500

    news page started

--- about.php
+++ about.php
@@ -5,9 +5,9 @@ include __DIR__.'/tmp/header.php'
 
 <div class="container-fluid">
 
-    <h1 class="about">О компании</h1>
+    <h1 class="page-h1">О компании</h1>
     <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 about-top"></div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 top-banner about-top"></div>
     </div>
     <div class="row row-about-text">
         <div class="col-lg-6 col-md-10 col-sm-10 col-xs-12 about-text">
--- news.php
+++ news.php
@@ -1,7 +1,65 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Валерий
- * Date: 20.06.2016
- * Time: 12:05
- */
+include __DIR__.'/tmp/init.php';
+include __DIR__.'/tmp/header.php'
+?>
+
+<div class="container-fluid">
+
+    <h1 class="page-h1">Новости</h1>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 top-banner news-top">
+            <div class="img-on-top"></div>
+        </div>
+    </div>
+
+    <div class="row news-block">
+        <div class="container-fluid">
+        
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 col-left">
+                    <a href="#">
+                        <div class="news-item">
+                            <div class="m-cat-img news-img" style="background-image: url('/img/car-big.jpg')"></div>
+                            <div class="m-cat-desc news-desc">
+                                <div class="news-date">11.04</div>
+                                Как избежать проблем с установкой
+                                видеорегистратора.</br>
+                                Советы от наших блогеров
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="news-item"></div>
+                    </a>
+                </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 col-mid">
+                    <a href="#">
+                        <div class="news-item">
+                            <div class="m-cat-img news-img" style="background-image: url('/img/car-big.jpg')"></div>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="news-item"></div>
+                    </a>
+                </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 col-right">
+                    <a href="#">
+                        <div class="news-item">
+                            <div class="m-cat-img news-img" style="background-image: url('/img/car-big.jpg')"></div>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="news-item"></div>
+                    </a>
+                </div>
+        
+        </div>
+    </div>
+    <div class="cb"></div>
+
+    <a class="back-on-top"><img src="img/back-on-top.png"></a>
+    <div class="back-on-top-static"></div>
+
+</div>
+
+
+<?php include __DIR__.'/tmp/footer.php'?>
