115c754529087300d07a03f0c5c35169a737cfa3
Vasiliy Tsykin <vasiliytsykin@gmail.com>
2016-07-26 20:47:43 +0500

    video block refactoring started

---
+++ tmp/video_block_templ.php
@@ -0,0 +1,19 @@
+<div class="row row-video">
+    
+    <div class="video-container">
+        
+        <div class="video"></div>
+
+        <div class="video-text">
+            <div class="tc">
+                <div class="td">
+                    <p>
+                        <?=$videoText;?>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    
+</div>
