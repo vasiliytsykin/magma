fcc74dd7be62024602f634064c72b77a16ae0744
Vasiliy Tsykin <vasiliytsykin@gmail.com>
2016-07-26 19:22:14 +0500

    fberertb

---
+++ single_new.php
@@ -0,0 +1,101 @@
+<?php
+include __DIR__.'/tmp/init.php';
+include __DIR__.'/tmp/header.php';
+
+?>
+
+
+<div class="container-fluid">
+
+    <div class="news-date">12.05</div>
+    <h1 class="about-h1">Ночью тоже можно видеть</h1>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 top-banner single-new-top"></div>
+    </div>
+    <div class="row row-about-text">
+        <div class="col-lg-6 col-md-10 col-sm-10 col-xs-12 about-text single-new-text">
+            <p class="about">
+                Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+                Рекомендованная розничная цена Magma iOne-1000 составляет 9790 рублей,
+                что тоже, кстати, можно назвать фирменной «фишкой» устройства.
+                Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+                Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+
+            </p>
+            <p class="about ione">
+                Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+                Рекомендованная розничная цена Magma iOne-1000 составляет 9790 рублей,
+                что тоже, кстати, можно назвать фирменной «фишкой» устройства.
+            </p>
+        </div>
+    </div>
+
+
+    <div class="row row-video">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 video-container">
+            <div class="video"></div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 video-text">
+            <div class="tc">
+                <div class="td">
+                    <p>
+                        Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row row-about-text row-single-new-text">
+        <div class="col-lg-6 col-md-10 col-sm-10 col-xs-12 about-text single-new-text">
+            <p class="about">
+                Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+                Рекомендованная розничная цена Magma iOne-1000 составляет 9790 рублей,
+                что тоже, кстати, можно назвать фирменной «фишкой» устройства.
+                Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+                Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+
+            </p>
+            <p class="about ione">
+                Magma iOne-1000 расширяет функционал и делает устройство максимально удобным для пользователя.
+                Рекомендованная розничная цена Magma iOne-1000 составляет 9790 рублей,
+                что тоже, кстати, можно назвать фирменной «фишкой» устройства.
+            </p>
+        </div>
+    </div>
+
+    <div class="single-new-img-wrapper">
+        <img src="/img/news-top-banner-cut.jpg" class="single-new-img">
+        <img src="/img/news-top-banner-cut.jpg" class="single-new-img">
+    </div>
+
+
+    <section class="news-footer">
+        <div class="tc">
+            <div class="td">
+                <div class="catalog-footer-text news-footer-text">
+                    <span class="name">Magma WIDE S50 </span><span class="text">теперь</br>
+                        доступен и в Екатериинбурге.</span></br>
+                    <a href="#" class="btn btn-grey buynow">купить сейчас</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="news-pager-wrapper">
+        <div class="simple-pager">
+            <ul class="pager-menu">
+                <li class="pager-menu-item"><a href="#" class="arrow-left"></a></li>
+                <li class="pager-menu-item"><a href="#">1</a></li>
+                <li class="pager-menu-item"><a href="#">2</a></li>
+                <li class="pager-menu-item"><a href="#">3</a></li>
+                <li class="pager-menu-item">...</li>
+                <li class="pager-menu-item"><a href="#">12</a></li>
+                <li class="pager-menu-item"><a href="#" class="arrow-right"></a></li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+<?php include __DIR__.'/tmp/footer.php'?>
