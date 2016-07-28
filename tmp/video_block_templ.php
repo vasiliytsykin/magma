7697a9119be7e5720f697914f54628f879acc174
Vasiliy Tsykin <vasiliytsykin@gmail.com>
2016-07-28 11:56:20 +0500

    video block refactored

--- tmp/video_block_templ.php
+++ tmp/video_block_templ.php
@@ -1,10 +1,12 @@
 <div class="row row-video">
     
-    <div class="video-container">
+    <div class="col-lg-6 col-md-10 col-sm-10 col-xs-12 video-container">
         
-        <div class="video"></div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 video-wrapper">
+            <div class="video"></div>
+        </div>
 
-        <div class="video-text">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 video-text">
             <div class="tc">
                 <div class="td">
                     <p>
